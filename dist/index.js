f8096aa9edc7aa3a39e9c91f220da471ce9338e1
Arturas Molcanovas <a.molcanovas@gmail.com>
2019-09-12 23:00:11 +0100

    inputs check

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
-const gh = tslib_1.__importStar(require("@actions/github"));
-const fs = tslib_1.__importStar(require("fs"));
-function sortObj(inp) {
-    return Object.keys(inp).sort().reduce((acc, k) => {
-        acc[k] = inp[k];
-        return acc;
-    }, {});
-}
-console.log(require('util').inspect({
-    cwd: process.cwd(),
-    ls: fs.readdirSync(process.cwd()),
-    dirLs: fs.readdirSync(process.env.GITHUB_WORKSPACE),
-    env: sortObj(process.env),
-    ctx: sortObj(gh.context)
-}, { colors: true, depth: 100 }));
+const core = tslib_1.__importStar(require("@actions/core"));
+console.log(core.getInput('gh_token', { required: true }));
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-import * as gh from '@actions/github';
-import * as fs from 'fs';
+import * as core from '@actions/core';
 
-function sortObj(inp: any): any {
-  return Object.keys(inp).sort().reduce(
-    (acc: any, k: string) => {
-      acc[k] = inp[k];
-
-      return acc;
-    },
-    {}
-  );
-}
-
-console.log(require('util').inspect(
-  {
-    cwd: process.cwd(),
-    ls: fs.readdirSync(process.cwd()),
-    dirLs: fs.readdirSync(<string>process.env.GITHUB_WORKSPACE),
-    env: sortObj(process.env),
-    ctx: sortObj(gh.context)
-  },
-  {colors: true, depth: 100}
-));
+console.log(core.getInput('gh_token', {required: true}));
